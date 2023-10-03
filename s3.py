b643319531ab8a716cc6f6cee4726a9bd0b6c0ef
Gustavo Barros <gustavo.barros@jurus.com.br>
2023-10-03 15:54:15 -0300

    fix s3

--- s3.py
+++ s3.py
@@ -5,12 +5,14 @@ load_dotenv()
 
 import os
 
-AWS_ACCESS_KEY_ID = os.getenv("AKIA5V4H4C5INBPTMLDW")
-AWS_SECRET_ACCESS_KEY = os.getenv("Ko3CQMxJDKNLHt7Jl8")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+S3_BUCKET = 'notas-dev-s3'
 
 s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-#uploads/dokki/client/66cc52e8-42af-4d22-bf38-4d0beefff6cc.jpg
-
 def download(key, filetemp):
     s3_client.download_file(Bucket='notas-dev-s3', Key=key, Filename=filetemp)
+
+def upload(file,key):
+    s3_client.upload_fileobj(file, S3_BUCKET, key)
--- server.py
+++ server.py
@@ -1,12 +1,41 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect
 from ocr.ocr import ocr_image
 from ocr.parser import Parser
 from flask import Flask
 from flask_cors import CORS
+import uuid
+import os
+from s3 import upload
+
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    file_extension = os.path.splitext(file.filename)[1]
+    filename = str(uuid.uuid4()) + file_extension
+
+    agent = request.args.get('agent')
+    sender = request.args.get('sender')
+
+    key = agent + "/" + sender + "/" + filename
+    print("key: " + key)
+
+    if file:
+        try:          
+            upload(file, key) 
+            return 'File uploaded successfully!!!' + file.filename
+        except Exception as e:
+            return str(e)
 
 @app.route('/ocr', methods=['GET'])
 def get_ocr():
